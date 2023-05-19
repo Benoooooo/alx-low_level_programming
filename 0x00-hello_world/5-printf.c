4bf7bceb9701d82e28a21a8f5bcbe0ce97c45fea
kelechi nnadi <kelechinnadi019@gmail.com>
2023-05-19 06:49:24 -0700

    the printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the code out
+ *
+ * printf@ prints the code  to standard output
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
