a076bba38a93cf2ee4e1141c198ee3e3b0edd627
kelechi nnadi <kelechinnadi019@gmail.com>
2023-05-19 21:52:18 -0700

    the positive number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,9 @@
 #include <stdio.h>
 /**
 * main - function that checks for random number
-* n@ is the number that check for conditions
-* Return: 0
+*
+* Return: positve if the number is grater than 0,
+* nagative if less than 0, or return 0;
 */
 int main(void)
 {
@@ -13,16 +14,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-    {
-       printf("%d is positive\n", n);
-    }
-    else if (n < 0)
-    {
-        printf("%d is negative\n", n);
-    }
-    else 
-    {
-       printf("%d is zero\n", n);
-    }
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
+
 }
