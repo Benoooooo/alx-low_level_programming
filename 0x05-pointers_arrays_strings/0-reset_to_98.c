d2f585b6452b2f0793ec459cd30cf9c55fffdbcd
kelechi nnadi <kelechinnadi019@gmail.com>
2023-05-29 22:01:29 -0700

    the pareameter

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * resest_to _98 - takes a pointer to an in to as parameter
  * @n: is the pointer to 98
- * @n: the vaslue to return
+ * @98: the vaslue to return
  */
 void reset_to_98(int *n)
 {
