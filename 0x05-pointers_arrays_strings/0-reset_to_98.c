897cc49ae3fad45bfee628d271e5dfa4266da327
kelechi nnadi <kelechinnadi019@gmail.com>
2023-05-29 21:59:49 -0700

    the

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * resest_to _98 - takes a pointer to an in to as parameter
+ * @n: is the pointer to 98
+ * @n: the vaslue to return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
