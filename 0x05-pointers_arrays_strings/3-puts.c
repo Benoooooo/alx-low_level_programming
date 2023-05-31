fe0cd0ffbb9731b83a906dc0776dc9fdef338034
kelechi nnadi <kelechinnadi019@gmail.com>
2023-05-30 21:22:46 -0700

    the commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - the function that print the string
+ * @str: the string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
