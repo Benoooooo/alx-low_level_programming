afe3b92503bfa864eae048dd99351b31412ffb74
kelechi nnadi <kelechinnadi019@gmail.com>
2023-05-30 21:45:24 -0700

    the line is here

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - function that print the strin in reverse
+ * @s: the string in revers
+ */
+void print_rev(char *s)
+{
+	int lent = 0;
+	int a;
+
+	while (*s != '\0')
+	{
+		lent++;
+		s++;
+	}
+	s--;
+	for (a = lent; a >= 0; a--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
