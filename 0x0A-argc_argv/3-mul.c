70291a3de4f5890f935d216a752b9787a38b4966
kelechi nnadi <kelechinnadi019@gmail.com>
2023-06-14 10:54:36 +0100

    the multilple

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints multiple numbers
+ * @argc: the number to ber counted in the line
+ * @argv: the arguments to be returned
+ */
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	int a, b, sum;
+		
+	if (argc != 3)
+	{ 
+		printf("Error\n");
+		return (1);
+	}
+	else 
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		sum = a * b;
+		printf("%d\n", sum);
+	}
+	return (0);
+}
