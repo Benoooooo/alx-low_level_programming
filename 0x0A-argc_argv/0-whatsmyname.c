305c74cc6cd09b392efa7dae07fe5e59b85e4121
kelechi nnadi <kelechinnadi019@gmail.com>
2023-06-14 07:34:59 +0100

    my name print

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * whatsmyname - print a names imputed to it
+ */
+
+int main(__attribute__((unused))int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints th arguments passed into it
+ */
+
+int main(int argc,  char* argv[]__attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	 printf("%d\n", arg
+			 );
+	}
+	 
+	 return 0;
+}
