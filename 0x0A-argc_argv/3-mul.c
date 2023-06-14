5dcac6d24214fa779d96e5482d1b0c13a315473d
kelechi nnadi <kelechinnadi019@gmail.com>
2023-06-14 11:04:07 +0100

    the mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - program that prints multiple numbers
- * @argc: the number to ber counted in the line
- * @argv: the arguments to be returned
- */
+* main - program that prints multiple numbers
+* @argc: the number to ber counted in the line
+* @argv: the arguments to be returned
+* Return: always 0;
+*/
 
-int main(int argc, char *argv[]__attribute__((unused)))
+int main(int argc, char *argv[])
 {
 	int a, b, sum;
-		
+
 	if (argc != 3)
-	{ 
+	{
 		printf("Error\n");
 		return (1);
 	}
-	else 
+
+	else
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
