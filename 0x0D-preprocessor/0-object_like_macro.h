5c0eadc75bb6c9d4440c6ffbe17558c722786827
kelechi nnadi <kelechinnadi019@gmail.com>
2023-06-20 11:16:37 +0100

    the define

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * FILE: object_like_macro.h
+ * AUTH: kelechi
+ * Desc: header that defines the macro name
+ * SIZE as an abbreviation for the token 1024
+ */
+#define SIZE 1024
+
+#endif
