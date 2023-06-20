45ec8de4edb5fe5abd8cb004726d8d8b278b6009
kelechi nnadi <kelechinnadi019@gmail.com>
2023-06-20 13:58:12 +0100

    the function

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/*
+ * file: function-like-macro.h
+ * Auth: kelechi
+ * Desc: function like macro
+ * ABS that computes the absolute value of number x
+ */
+#define ABS(x) ((-x) < (x) ? (x) : (-x))
+#endif
