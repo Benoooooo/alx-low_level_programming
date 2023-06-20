2fe74b6052b0ea3d552d55a9fa56b2f1b7691ef8
kelechi nnadi <kelechinnadi019@gmail.com>
2023-06-20 14:43:42 +0100

    the sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: sum.h
+ * Auth: kelehi
+ * Desc: function-like macro
+ * SUM that computes the sum of the numbers x and y
+ */
+#define SUM(x, y) (x + y)
+#endif
