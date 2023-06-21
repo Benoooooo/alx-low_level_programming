0e2c1cfad8b0b22b076f21f9ad267705cc446ac7
kelechi nnadi <kelechinnadi019@gmail.com>
2023-06-21 03:59:02 +0100

    the main.c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *strunct dog - that  check to see how struc
+* @mame:  the name of the dog
+* @age - the age to the dog
+* @mame  - the name of the owner; 
+ */
+struct dog
+{
+	char *name;
+	float  age;
+	char *owner;
+};
