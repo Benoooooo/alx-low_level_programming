9632d77cc6f5e16c1c5b40a39ac47b1a2199e31b
kelechi nnadi <kelechinnadi019@gmail.com>
2023-06-21 04:56:24 +0100

    the dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,18 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 /**
- *strunct dog - that  check to see how struc
-* @mame:  the name of the dog
-* @age - the age to the dog
-* @mame  - the name of the owner; 
- */
+* struct dog -  sturct dog
+* @name: stores the name of the dog
+* @age:  stores the age to the dog
+* @owner: stores the owner information with pointer
+*
+* Description: the header that stores the dogs information
+* adding dog name, age of the dog, the owner name
+*/
 struct dog
 {
 	char *name;
 	float  age;
 	char *owner;
 };
+#endif
