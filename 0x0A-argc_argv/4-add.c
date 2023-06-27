6039c9e9118f81ea86caf2a9935aa6928002f4d7
kelechi nnadi <kelechinnadi019@gmail.com>
2023-06-27 07:55:05 +0100

    the code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - program that add positive numbers
- * @argc: the number that count the line
- * @argv: the string to add in form of number
- * Return: always 0;
- */
+* main - Prints the addition of positive numbers,
+*        followed by a new line.
+* @argc: the number fo arguments passed to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: If one of the numbers contains symbols that are non-digits - 1.
+*         Otherwise - 0.
+*/
 int main(int argc, char *argv[])
 {
-	int num, digit, sum = 0;
+int num, digit, sum = 0;
 
-	for (num = 1; num < argc; num++)
-	{
-		for (digit = 0; argv[num][digit]; digit++)
-		{
-			if (argv[num][digit] < '0' || argv[num][digit] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		sum += atoi(argv[num]);
-	}
-	printf("%d\n", num);
-	return (0);
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+
+sum += atoi(argv[num]);
+}
+
+printf("%d\n", sum);
+
+return (0);
 }
