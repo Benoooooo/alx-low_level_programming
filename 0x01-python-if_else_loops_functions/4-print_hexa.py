33aefd28affa680653fe22bdb5c8749d5f5f9a26
kelechi nnadi <kelechinnadi019@gmail.com>
2023-10-13 01:38:10 +0100

    the code

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
-    print('{} ={}'.format(i, chr[)]))
+# file: 4-print_hexa.py
+# Auth: kelechi nnadi <alx swe>
+
+for i in range(0, 99):
+    print("{} = {}".format(i, hex(i)))
