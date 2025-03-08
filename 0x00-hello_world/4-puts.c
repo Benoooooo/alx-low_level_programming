9b3b6857b74869de3364f707b439a284ded5b098
kelechi nnadi <kelechinnadi019@gmail.com>
2025-03-08 05:38:53 +0100

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - function that prints exactly programming is like multi puzzle,
+ *	  followed by new line
+ * Return: Always (0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
