cfd2b532e9897740b1a8a09f6e412c0bc65a5e6f
kelechi nnadi <kelechinnadi019@gmail.com>
2025-03-08 06:04:49 +0100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ *
+ * Description: this program prints c program with exactly the output
+ * Return: always (0);
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
