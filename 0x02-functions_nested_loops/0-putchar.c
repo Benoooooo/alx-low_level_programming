3e2699cb26dc5b11a50a3234b9c91b76e45d88c6
kelechi nnadi <kelechinnadi019@gmail.com>
2025-03-12 19:49:55 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This code prints _putchar followed by a new line
+ *
+ * Return: the program should return 0
+ */
+
+int main(void)
+{
+	int _putchar(char c);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
