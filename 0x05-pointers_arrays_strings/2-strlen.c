9a24a01a7ecdf89bc19a11b104d9d535fc0f6641
kelechi nnadi <kelechinnadi019@gmail.com>
2025-03-27 21:37:25 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: the s is the character pointer
+ * Return: the lent of the string
+ */
+int _strlen(char *s)
+{
+	int len = strlen(s);
+
+	return (len);
+}
