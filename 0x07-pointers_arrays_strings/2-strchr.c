90fa05457f831366bd5b05ec22c8684486b0fa77
kelechi nnadi <kelechinnadi019@gmail.com>
2025-04-25 05:35:02 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - function that locates ta character in a string
+ * @s: is the pointer to the string address
+ * @c: is the character to be find
+ * Return: a pointer to the first occurance of the character
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	
+	return (NULL);
+}
