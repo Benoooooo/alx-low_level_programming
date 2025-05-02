488dbb293e985c7759ad39156e65755f17ef4cb6
kelechi nnadi <kelechinnadi019@gmail.com>
2025-05-02 16:25:30 +0100

    the code

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ * @argc: is the count but unsused for now
+ * @argv: is the main one that prints the name add a new line
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
