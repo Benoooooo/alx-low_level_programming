49038c03afc9ef74c66c6f7d04ec3706ad66fef8
kelechi nnadi <kelechinnadi019@gmail.com>
2025-05-02 06:53:48 +0100

    the code

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,39 @@
 #include <stdio.h>
+#include "main.h"
+#include <limits.h>
+/**
+ * _atoi - function that convert a string to and integer
+ * @s: is the string pointer
+ * Return: the int
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int digit_seen = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s == '+')
+			;
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit_seen = 1;
+
+			if (result > (INT_MAX / 10) ||
+					(result == (INT_MAX / 10) && (*s - '0') > (INT_MAX % 10)))
+					{
+					return ((sign == 1) ? INT_MAX : INT_MIN);
+					}
+					result = result * 10 + (*s - '0');
+					}
+					else if (digit_seen)
+					{
+					break;
+					}
+					s++;
+					}
+					return ((digit_seen) ? result * sign : 0);
+}
