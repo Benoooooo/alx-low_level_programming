551aca72f2344301db766d9d5205b30abf747624
kelechi nnadi <kelechinnadi019@gmail.com>
2025-05-11 19:52:40 +0100

    the code

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,16 +16,19 @@ char *_strdup(char *str)
 {
 	char *ptr;
 	int i;
+	int len = 0;
 
 	if (str == NULL)
 		return (NULL);
+	while (str[len] != '\0')
+		len++;
 
-	ptr = malloc(sizeof(char) + 1);
+	ptr = malloc(sizeof(char)  * (len + 1));
 
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; str[i]; i++)
+	for (i = 0; i <= len; i++)
 	{
 		ptr[i] = str[i];
 	}
