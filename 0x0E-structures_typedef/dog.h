f6f6326233faedac124216bd07ad0f7036df0a64
kelechi nnadi <kelechinnadi019@gmail.com>
2025-06-02 12:03:15 +0100

    struch dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog  - define a new types with elements
+ * @name: is the name of the dog
+ * @age: is the age of the dog
+ * @owner: is the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
