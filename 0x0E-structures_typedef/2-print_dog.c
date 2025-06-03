0201ccaafed0169819eae29c496e37a17ecfbc8e
kelechi nnadi <kelechinnadi019@gmail.com>
2025-06-03 07:22:17 +0100

    owner

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,21 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %.1f\n", d->age);
-	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	if (d->name == NULL)
+	{
+		printf("name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n",  d->name);
+	}
+	printf("age: %.1f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n",  d->owner);
+	}
 }
