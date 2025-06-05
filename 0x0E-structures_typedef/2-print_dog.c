b33914f2af91a4929d6b16bcdf889768cb95e1e3
kelechi nnadi <kelechinnadi019@gmail.com>
2025-06-05 07:00:04 +0100

    the code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 	{
-		printf("Name:(nil)\n");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
@@ -26,7 +26,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 	{
-		printf("Name:(nil)\n");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
